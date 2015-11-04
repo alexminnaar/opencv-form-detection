777e6e3d062cc6acbbf419f031b1b249eb2e7910
Alex Minnaar <Alex.Minnaar@gonitro.com>
2015-11-04 10:39:24 -0800

    Initial Commit

---
+++ opencv-table-detection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv-table-detection
+//
+//  Created by Alex Minnaar on 11/4/15.
+//  Copyright (c) 2015 Alex Minnaar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
